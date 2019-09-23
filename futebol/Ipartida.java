68a74d0ae0776a97b5bcdfa3831d2c73b912e576
nicholasmonteiro <48843338+nicholasmonteiro@users.noreply.github.com>
2019-09-23 20:32:18 -0300

    Create Ipartida.java

---
+++ futebol/Ipartida.java
@@ -0,0 +1,15 @@
+public interface Ipartida {
+	
+	
+	
+			 
+		
+		public abstract void iniciarPartida();
+		
+		public abstract void mostrarResultado();
+		
+		public abstract void finalizarPartida();
+		
+		
+
+	}
