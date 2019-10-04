a2bace39b386fee7029b8446c14fe7b31d75d391
nicholasmonteiro <48843338+nicholasmonteiro@users.noreply.github.com>
2019-10-04 16:38:45 -0300

    Update TesteTwitter.java

--- AlunoT/src/Trabalho2/TesteTwitter.java
+++ AlunoT/src/Trabalho2/TesteTwitter.java
@@ -1,3 +1,17 @@
+
+Crie 3 Classes: Usuario, Tweet, TestTweet.
+Faça um programa(TestTweet) para pedir login e senha do usuario (Atributos da Classe Usuário) e compare com a senha "poo".
+Após o login, peça com que ele entre com o Tweet (texto de 140 caracteres) e pergunte se deseja twitar novamente.Se usuário escolher sim, você deve continuar pedindo o texto do Tweet. Ao final do programa, você deve listar : login e todos os tweetes que foram inseridos no programa (use ArrayList). PAra isso, crie um método listar dentro da classe Usuario.
+Resumindo o modelo de Classes: Um Usuário pode ter um ou mais Tweets
+
+Observações:
+1- Deixar todos os atributos privados. Usar Getter/Setter para alterar os mesmos.
+2-No construtor, crie um atributo senha e inicial e com "OOP". Esta deverá ser a senha de comparação no momento do login
+3-Estou anexando a explicação do diagrama de Classes comentado ontem em sala de aula.
+4-Comentem o código de vocês, coloque o nome e matricula no cabeçalho das Classes.
+
+
+
 //matricula 0050011999//
 // nicholas monteiro//
 
